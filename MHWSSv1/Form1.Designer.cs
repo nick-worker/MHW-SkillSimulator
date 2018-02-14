aca3ecc26d6f2a80e8f3e6e1eab2567f0010ce7b
nick <s_nare1@ybb.ne.jp>
2018-02-14 16:23:10 +0900

    スキルツリーで選択したスキルをリストボックスに表示 リストボックスでスキルを選択して追加ボタンを押すとグリッドに表示

--- MHWSSv1/Form1.Designer.cs
+++ MHWSSv1/Form1.Designer.cs
@@ -29,23 +29,61 @@
         private void InitializeComponent()
         {
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
             // 
             this.treeView1.Location = new System.Drawing.Point(12, 12);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(204, 320);
+            this.treeView1.Size = new System.Drawing.Size(204, 353);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(222, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(293, 235);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(222, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 112);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 449);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -53,6 +91,9 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MHWSSv1/Form1.cs
+++ MHWSSv1/Form1.cs
@@ -16,7 +16,14 @@ namespace MHWSSv1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "MHWSS v1";
+            button1.Text = "追加";
             ImportSkillXml("../../xml/SkillList/mhwss-skill.xml");
+            SettingDataGridView1();
         }
 
         private void ImportSkillXml(String xmlPath)
@@ -66,12 +73,14 @@ namespace MHWSSv1
                     }
                     // name属性を取得
                     sni.Name = ((XmlElement)childXmlNode).GetAttribute("name");
+                    sni.Limit = ((XmlElement)childXmlNode).GetAttribute("limit");
+                    sni.Need = ((XmlElement)childXmlNode).GetAttribute("need");
 
                     // TreeNodeを作成
                     TreeNode tn = new TreeNode(sni.Name);
 
                     tn.Tag = sni;
-                    
+                                        
                     // 親ノードに子ノードを追加
                     ParenttreeNode.Nodes.Add(tn);
 
@@ -82,5 +91,68 @@ namespace MHWSSv1
             }
         }
 
+        // DataGridView1 のセッティング
+        private void SettingDataGridView1()
+        {
+            dataGridView1.ReadOnly = true;
+            DataGridViewTextBoxColumn textColumn1 = new DataGridViewTextBoxColumn();
+            textColumn1.DataPropertyName = "Column1";
+            textColumn1.Name = "Number";
+            textColumn1.HeaderText = "No";
+            textColumn1.SortMode = DataGridViewColumnSortMode.NotSortable;
+            textColumn1.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns.Add(textColumn1);
+
+            DataGridViewTextBoxColumn textColumn2 = new DataGridViewTextBoxColumn();
+            textColumn2.DataPropertyName = "Column2";
+            textColumn2.Name = "Skill";
+            textColumn2.HeaderText = "スキル";
+            textColumn2.SortMode = DataGridViewColumnSortMode.NotSortable;
+            textColumn2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns.Add(textColumn2);
+
+            DataGridViewTextBoxColumn textColumn3 = new DataGridViewTextBoxColumn();
+            textColumn3.DataPropertyName = "Column3";
+            textColumn3.Name = "Value";
+            textColumn3.HeaderText = "値";
+            textColumn3.SortMode = DataGridViewColumnSortMode.NotSortable;
+            textColumn3.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.Columns.Add(textColumn3);
+            //dataGridView1.Row
+            dataGridView1.RowCount = 10;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+
+            dataGridView1.RowHeadersVisible = false;
+        }
+
+        // treeViewで選択したスキルをlistBoxに表示
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listBox1.Items.Clear();
+            SkillNodeInfo sni = (SkillNodeInfo)treeView1.SelectedNode.Tag;
+            
+            // itemの情報のみlistBox1にセット
+            if(sni.Type == "item")
+            {
+                int limit = int.Parse(sni.Limit);
+                for(int i = 0; i < limit; i++)
+                {
+                    int j = limit - i;
+                    listBox1.Items.Add(sni.Name + "+" + j);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem != null)
+            {
+                String selectSkill = listBox1.SelectedItem.ToString();
+                dataGridView1[0, 0].Value = "1";
+                dataGridView1[1, 0].Value = selectSkill;
+            }
+
+        }
     }
 }
